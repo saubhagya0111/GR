c19a66fa5a0a7f74fd314fa9c40163a42b3922e5
saubhagya0111 <chopragolu2345@gmail.com>
2024-09-03 22:07:31 +0530

    ran db migrations

--- gr/quotes/models.py
+++ gr/quotes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    biography = models.TextField(null=True, blank=True)
+
+class Quote(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    tags = models.CharField(max_length=200, null=True, blank=True)
